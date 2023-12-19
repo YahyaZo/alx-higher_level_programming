4ad72cc978266b0c8aba189279ead6d39cd2d760
YahyaZo <khayoussef.yahya20@gmail.com>
2023-12-19 21:28:35 +0100

    Mandatory + Advanced tasks

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""class square"""
+
+
+class Square:
+    """private instance and raise errors for exception"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
